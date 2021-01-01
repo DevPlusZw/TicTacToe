c057929daa3a75b5edc5a09802f22f215e43abf2
devpluszw <devpluszw@gmail.com>
2021-01-01 08:35:05 +0200

    myg

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from games import game
 from flask import Flask, session, request, redirect, url_for, render_template
+
 app = Flask(__name__)
 app.secret_key = b'_5#Gc2L"F4Q8z\n\Mec]/'
 
@@ -7,20 +8,43 @@ mysession = 'mysession'
 error = '' #set to pass errors to UI
 tictac = game()
 pos = 0
+
+@app.before_request
+def starter():
+    # having fun with request headers
+    toV2 = request.headers
+    print(tictac.passw)
+    allowed = ['gameover', 'gamon']
+    if "Sec-Ch-Ua-Platform" in toV2:
+        SvG9 = toV2["Sec-Ch-Ua-Platform"]
+        btv1 = SvG9.replace('"', '')
+        print(btv1)
+        if btv1 != "macOS":
+            return "invalid btv1"
+    print(toV2)
+    if request.endpoint not in allowed:
+        return "<h1>Not allowed to view this page</h1>"
+    print("before")
+   
+    
+
 @app.errorhandler(404)
 def page_not_found(e):
-    return "<center><h4>Page Youre looking for doesnt exist - DevPlusZw <a href='/'>go back</></h4></center>"
+    return "<center><h4>Page Youre looking for doesnt exist - DevPlusZw <a href='/'>go back</a></h4></center>"
+
+
 @app.route('/', methods=['GET', 'POST'])
 def gamon():
     session['game'] = mysession
+    print(session['game'])
     error = ''
-    player = tictac.get_player()
     data = tictac.start()
     board = data['1']
     num = data['2']
+    player = tictac.get_player()
     pos = 0
     if request.method == 'POST':
-        player = request.form['player']
+        player = tictac.get_player()
         if tictac.check_gameon():
             if request.form['pos'] == "":
                 print('error on pos')
@@ -33,23 +57,33 @@ def gamon():
                         #for win
                         error = "Gameover! " + str(player) + " won."
                         print("win")
+                        session.pop('game', None)
+                        tictac.restart()
+                        return render_template('index.html', board=board, player=player, num=num, error=error, over='over')
                     elif tictac.check_win() == "draw":
                         error = "Draw! its a Tie"
+                        session.pop('game', None)
+                        tictac.restart()
+                        return render_template('index.html', board=board, player=player, num=num, error=error, over='over')
                         print("draw")
-                        
                     else:
                         print("gameon")
                         #for draw
-                    data =tictac.start()
-                    num = data['2']
-                    tictac.switch()
+                        data =tictac.start()
+                        num = data['2']
+                        tictac.switch()
+                        player = tictac.get_player()
                 else:
                     error = "IVALID POSITION PLAYED, TRY AGAIN"
             return render_template('index.html', board=board, player=player, num=num, error=error)
     return render_template('index.html', board=board, player=player, num=num, error=error)
-@app.route('/gameover')
+
+
+@app.route('/Score')
 def gameover():
-    return "game over" 
+    tictac.gameon = False
+    print("game over") 
+    return "<h1><u>Score Board | TicTacToe by DevPlusZw</u> &copy 2022 - DevPlusZw <a href='/'>go back</a></h1>" 
 # game funtion
 
 if __name__ == "__main__":
--- games.py
+++ games.py
@@ -1,19 +1,22 @@
 class game:
+    passw = 'i will never use this pass....'
     def __init__(self):
         self.board = [" ", " ", " ", " ", " ", " ", " ", " ", " "] #3 rows and 3 colums for the game
 
         self.player1 = "X"
         self.player2 = "0"
-        self.turn = self.player1 
+        self.turn = self.player1
         self.winner = None
         self.count = 0
         self.gameon = True
+
+
     def start(self):
-        
         y = self.count
         return {"1" : self.board , "2" : y}
         # return "titac" + str(self.count)#just for testing function call
         #start the game logic
+
 
     def move_valid(self, pos):
         if pos >= 0 and pos <= 8 and self.board[pos] == " ":
@@ -24,39 +27,64 @@ class game:
 
     def get_player(self):
         return self.turn
+
+
     def check_win(self):
-        if self.board[0] == self.board[1] == self.board == self.board[2] == "X": #horizontal 1 check
-               self.gameon = False
-               return "win"
-        if self.board[0] == self.board[1] == self.board == self.board[2] == "0": #horizontal 1 check
-               self.gameon = False
-               return "win"
-        if self.board[3] == self.board[4] == self.board == self.board[5] == "X": #horizontal 1 check
-               self.gameon = False
-               return "win"
-        if self.board[3] == self.board[4] == self.board == self.board[5] == "0": #horizontal 1 check
-               self.gameon = False
-               return "win"
-        if self.board[6] == self.board[7] == self.board == self.board[8] == "X": #horizontal 1 check
-               self.gameon = False
-               return "win"
-        if self.board[6] == self.board[7] == self.board == self.board[8] == "0": #horizontal 1 check
+        if (self.board[0] == self.board[1] == self.board[2]) and (self.board[0] == "X" or self.board[0] == "0"): #horizontal 1 check
                self.gameon = False
                return "win"
 
-        if self.count == 9:
+        if (self.board[3] == self.board[4] == self.board[5]) and (self.board[3] == "X" or self.board[3] == "0") : #horizontal 1 check
+               self.gameon = False
+               return "win"
+
+        if (self.board[6] == self.board[7] == self.board[8]) and (self.board[6] == "X" or self.board[6] == "0"): #horizontal 1 check
+               self.gameon = False
+               return "win"
+
+        if (self.board[0] == self.board[4] == self.board[8]) and (self.board[4] == "X" or self.board[4] == "0"): #horizontal 1 check
+               self.gameon = False
+               return "win"
+
+        if (self.board[6] == self.board[4] == self.board[2]) and (self.board[6] == "X" or self.board[6] == "0"): #horizontal 1 check
+               self.gameon = False
+               return "win"
+        
+        if (self.board[0] == self.board[3] == self.board[6])  and (self.board[3] == "X" or self.board[3] == "0"): #horizontal 1 check
+               self.gameon = False
+               return "win"
+            
+        if (self.board[1] == self.board[4] == self.board[7])  and (self.board[7] == "X" or self.board[7] == "0"): #horizontal 1 check
+               self.gameon = False
+               return "win"
+        
+        if (self.board[2] == self.board[5] == self.board[8])  and (self.board[8] == "X" or self.board[8] == "0"): #horizontal 1 check
+               self.gameon = False
+               return "win"
+
+        if self.count == 9: #check for draw
             self.gameon = False
             return "draw"
-        #check if game is over and someone won...add other check validations
+        #check win function is over
+
+
     def check_gameon(self):
         return self.gameon
+
     def apply_move(self, pos):
         self.board[pos] = self.turn
         self.count += 1
-        #input move to board
+        #input move to board and increase counter for drwa check
+
     def switch(self):
         if self.turn == self.player1:
-            self.turn == self.player2
+            self.turn = self.player2
+            print(self.turn)
         else:
-            self.turn == self.player
+            self.turn = self.player1
+            print(self.turn)
         return self.turn
+    def restart(self):
+        print("restart")
+        self.__init__()
+   
