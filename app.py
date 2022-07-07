43ade83e807a1ebcab2d9bb2eaae25f7b3fc821d
devpluszw <devpluszw@gmail.com>
2022-07-07 16:39:12 +0200

    r Please enter the commit message for your changes. Lines starting

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ app = Flask(__name__)
 
 @app.route('/') #corrected double route error
 def method_name():
-    return "TicTacToe Game By devplus"
+    return "<h1>TicTacToe Game By devplus</h1>"
     # to add template rendering for initial game
     # 
+@app.errorhandler(404):
+def page_not_found(e):
+    return "<center><h1>Hakuna Page Rakadaro</h1></center>"
 
 if __name__ == "__main__":
     app.run()
