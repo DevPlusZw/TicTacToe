7e5bd03f35911ee22b285a4b27230dc166a6aada
devpluszw <devpluszw@gmail.com>
2022-07-07 16:05:58 +0200

    sk template

---
+++ app.py
@@ -0,0 +1,8 @@
+from Flask import Flask
+app = Flask(__name__)
+
+@app.@app.route('/')
+def method_name():
+    return "TicTacToe Game By devplus"
+    # to add template rendering for initial game
+    # 
